bee3526c1fa348f336c814814233a2061024c5b4
Shanzida-Rifa <rifashanzida@gmail.com>
2021-09-01 19:41:38 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,37 @@
+const loadCountries = () => {
+    fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = countries => {
+    // for(const country of countries){
+
+    // }
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        const div = document.createElement('div');
+        div.classList.add('country')
+        div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onClick = "loadCountryByName('${country.name}')"> Details </button>
+            `;
+        countriesDiv.appendChild(div);
+    })
+}
+const loadCountryByName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]));
+}
+const displayCountryDetail = country => {
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+    <h5>${country.name}</h5>
+    <p>Population: ${country.population} </p>
+    <img width="200px" src="${country.flag}">
+    `;
+}
